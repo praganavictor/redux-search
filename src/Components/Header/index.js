1999572e989de44ed666c79bb562ab4eab1454e9
Victor Pragana <victormerigue@gmail.com>
2019-08-05 14:08:58 -0300

    setup initial

---
+++ src/Components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <>
+      <input type="text" />
+      <button>Search</button>
+    </>
+  );
+}
